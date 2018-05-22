329331a51fbecdd78ac78991e5b97fb13b650bfe
josephraguilar <josephraguilar@gmail.com>
2018-05-21 21:07:56 -0700

    day 1 takehome commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,5 @@
-var myName = "Ryeker"
+var myName = "Joe"
 console.log(myName);
+
+var aboutMe = "I want to be able to fully understand how javascript works and how its properly implemented in modern websites. My favorite hobbies are playing video games and building & learning about computers."
+console.log(aboutMe);
