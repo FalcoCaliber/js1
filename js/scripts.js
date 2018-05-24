672a7893def01167877e1f10039f33cde530378a
josephraguilar <josephraguilar@gmail.com>
2018-05-23 21:03:57 -0700

    day two ending commit

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+var myName = "Joe";
+let myBirthday = new Date("1.30.1995") ;
+const goal = "work in blockchain";
+
+console.log(myName, myBirthday, goal);
+
+let userAnswer = confirm("did you brush your teeth today?");
+
+
+console.log(userAnswer);
+
+let userPromptAnswer = prompt("Did you brish your teeth today?","Yes");
+
+// create two let variables, 1. favorite food 2. fav movie , two const variables, ingrediants to that favorite food "array", year the movie was released"use date object", confirm the movie, alert the year the movie came out, prompt the ingredients
+
+let favFood = "Sushi";
+let favMovie= "SuperBad";
+
+const sushiIngr = ["fish","Rice", "love"];
+const movieRel = new Date("8.17.2007");
+
+confirm(favMovie);
+alert("It came out "movieRel"!");
+prompt(favFood + sushiIng);
+
+// fmllllllllllll
